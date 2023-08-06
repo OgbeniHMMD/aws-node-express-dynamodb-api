890c0f1e54461856642406ca6265a52025e1e5e3
Hammed A. Olajide <olajide.a.hammed@gmail.com>
2023-08-06 06:38:36 +0100

    refactoring: improve ddb-client for local

--- config/dynamoDB.js
+++ config/dynamoDB.js
@@ -1,22 +1,21 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
 
-let dynamoDBClientQuery = {}
+let client = new DynamoDBClient();
 let USERS_TABLE = "users-table-dev"
 
 if (process.env.ENVIRONMENT == 'development') {
     USERS_TABLE = process.env.USERS_TABLE
-    dynamoDBClientQuery = {
+    client = new DynamoDBClient({
         region: process.env.AWS_REGION,
         endpoint: process.env.AWS_DDB_ENDPOINT,
         credentials: {
             accessKeyId: process.env.AWS_ACCESS_KEY_ID,
             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
         }
-    }
+    })
 }
 
-const client = new DynamoDBClient();
 const dynamoDBDocument = DynamoDBDocument.from(client);
 
 module.exports = {
