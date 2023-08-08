53c6eeec6475fbdb9ba40cc1c010256c3b369f6d
Hammed A. Olajide <olajide.a.hammed@gmail.com>
2023-08-08 06:01:37 +0100

    setup basic swaggerUI

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 require('dotenv').config()
+const path = require('path')
+
 const express = require("express");
 const serverless = require("serverless-http");
 
@@ -9,8 +11,9 @@ const usersRouter = require('./routers/usersRouter');
 const app = express();
 app.use(express.json());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/docs', express.static(path.join(__dirname, '/docs')))
 
 app.use((req, res, next) => {
   return res.status(404).json({
